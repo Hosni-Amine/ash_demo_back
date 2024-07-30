80b15f6c97691bbccd7053680fc44a65dc474304
Hosni-Amine <amine.hosni02@gmail.com>
2024-07-30 13:13:20 +0100

    Enable all CORS and update port configuration

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
-  await app.listen(4000);
+  app.enableCors(); // Enable all CORS
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
