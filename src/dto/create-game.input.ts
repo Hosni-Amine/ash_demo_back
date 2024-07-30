bbc1e5e08071fcdddad944da16d183aa799a7a4e
Hosni-Amine <amine.hosni02@gmail.com>
2024-07-30 12:43:59 +0100

    first commit

--- src/dto/create-game.input.ts
+++ src/dto/create-game.input.ts
@@ -1,30 +1,22 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsDateString, IsNumber, IsOptional, IsString } from 'class-validator';
 
 @InputType()
 export class CreateGameInput {
   @Field()
-  @IsDateString()
   startDate: Date;
 
   @Field()
-  @IsDateString()
   endDate: Date;
 
   @Field()
-  @IsNumber()
   price: number;
 
   @Field()
-  @IsNumber()
   usernumber: number;
 
   @Field()
-  @IsString()
   name: string;
 
   @Field(() => String, { nullable: true })
-  @IsOptional()
-  @IsString()
   posteId?: string; // Optional field to associate the game with a Poste
 }
--- src/dto/update-game.input.ts
+++ src/dto/update-game.input.ts
@@ -1,11 +1,8 @@
 import { Field, InputType, PartialType } from '@nestjs/graphql';
-import { IsOptional, IsString } from 'class-validator';
 import { CreateGameInput } from './create-game.input';
 
 @InputType()
 export class UpdateGameInput extends PartialType(CreateGameInput) {
   @Field(() => String, { nullable: true })
-  @IsOptional()
-  @IsString()
   posteId?: string;
 }
